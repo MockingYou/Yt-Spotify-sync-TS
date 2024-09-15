869002e7039b2fb670b3bd85de33d42a5ae97147
Vlad <vlad.mocanu13@gmail.com>
2024-09-15 12:33:52 +0300

    - routes updates

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,11 @@ import express, { Request, Response, Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import config from "./utils/config.json";
-import session from "express-session"
+import session from "express-session";
 
 import AuthData from "./utils/interfaces/AuthData";
 import authRouter from "./routes/authRoutes";
-import userRouter from "./routes/userRoutes"
+import userRouter from "./routes/userRoutes";
 import playlistRoutes from "./routes/playlistRoutes";
 
 import cookieParser from "cookie-parser";
@@ -16,27 +16,60 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 const uri = process.env.MONGO_URI as string;
 
-
-
 const corsOptions = {
 	origin: "http://localhost:5173",
 	credentials: true,
 };
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }as ConnectOptions);
-        console.log("MongoDB connected successfully");
-    } catch (err) {
-        console.error("MongoDB connection error:", err);
-        process.exit(1);
-    }
+const connectDB = async (uri: string) => {
+	await mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		connectTimeoutMS: 10000,
+		socketTimeoutMS: 45000,
+		retryWrites: true,
+	} as ConnectOptions);
+	console.log("MongoDB connected successfully");
 };
 
-connectDB();
+const retryConnectDB = async (uri: string, maxRetries = 50, delay = 1000) => {
+	let isConnected = false;
+	let i = 1;
+	try {
+		await connectDB(uri);
+		isConnected = true;
+	} catch (error) {
+		console.error("MongoDB connection error:", error);
+	}
+	const retryInterval = setInterval(async () => {
+		if (isConnected) {
+			clearInterval(retryInterval);
+			return;
+		}
+		try {
+			await connectDB(uri);
+			isConnected = true;
+		} catch (error) {
+			console.error("MongoDB connection error:", error);
+			console.info("Retry connection...");
+			if (maxRetries && i === maxRetries) {
+				console.error(
+					`Failed to connect after ${i} attemps. Exiting...`,
+				);
+				process.exit(1);
+			}
+			console.info(
+				`Retrying connection attemp ${i}/${maxRetries} in ${
+					delay / 1000
+				} seconds...`,
+			);
+		}
+		i++;
+	}, delay);
+};
+
+retryConnectDB(uri);
+
 app.disable("x-powered-by");
 
 app.use(cors(corsOptions));
@@ -45,11 +78,11 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use(
-    session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-    })
+	session({
+		secret: "secret",
+		resave: true,
+		saveUninitialized: true,
+	}),
 );
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -80,12 +113,9 @@ app.get("/events", cors(corsOptions), (req: Request, res: Response) => {
 	}, 1000);
 });
 
-
 app.use("/api", authRouter);
 app.use("/api", userRouter);
-app.use("/api", playlistRoutes)
-
-
+app.use("/api", playlistRoutes);
 
 app.listen(port, () => {
 	console.log(`Server start http://localhost:${port}`);
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { authenticateToken } from "../middleware/authMiddleware";
 import { loginHandler } from "../utils/methods/loginHandler";
 import Spotify from "../utils/spotify/Spotify";
 import Youtube from "../utils/youtube/Youtube";
 import config from "../utils/config.json";
 import { triggerLogout } from "../middleware/authMiddleware";
-import User from "../models/User"
+import User from "../models/User";
 
 const spotify_scopes = config.spotify.scopes;
 const jwtSecret = process.env.JWT_SECRET!;
@@ -122,56 +122,55 @@ router.get("/auth/spotify/callback", async (req, res) => {
 });
 
 router.post("/auth/login", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({
-            $or: [{ username }, { email: username }],
-        });
-		console.log(user)
-        if (!user) {
-            return res
-                .status(401)
-                .json({ message: "Invalid user", type: "ERROR" });
-        }
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res
-                .status(401)
-                .json({ message: "Invalid password", type: "ERROR" });
-        }
-        const token = jwt.sign({ userId: user._id }, "your-secret-key", {
-            expiresIn: "1h",
-        });
-        res.status(200).json({
-            message: "Login succesfull",
-            type: "SUCCESS",
-            token,
-            username: user.username,
-            email: user.email,
-            id: user._id,
-        });
-    } catch (error) {
-        res.status(500).json({ message: "Login failed", type: "ERROR" });
-    }
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({
+			$or: [{ username }, { email: username }],
+		});
+		console.log(user);
+		if (!user) {
+			return res
+				.status(401)
+				.json({ message: "Invalid user", type: "ERROR" });
+		}
+		const passwordMatch = await bcrypt.compare(password, user.password);
+		if (!passwordMatch) {
+			return res
+				.status(401)
+				.json({ message: "Invalid password", type: "ERROR" });
+		}
+		const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+			expiresIn: "1h",
+		});
+		res.status(200).json({
+			message: "Login succesfull",
+			type: "SUCCESS",
+			token,
+			username: user.username,
+			email: user.email,
+			id: user._id,
+		});
+	} catch (error) {
+		res.status(500).json({ message: "Login failed", type: "ERROR" });
+	}
 });
 
 // Add more error handling to signup
-router.post("/auth/signup", async(req, res) => {
+router.post("/auth/signup", async (req, res) => {
 	try {
-        const { username, password, email } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ username, password: hashedPassword, email });
-        await user.save();
-        res.status(201).json({
-            message: "User registered successfully",
-            type: "SUCCESS",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Registration failed", type: "ERROR" });
-    }
-})
-
+		const { username, password, email } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = new User({ username, password: hashedPassword, email });
+		await user.save();
+		res.status(201).json({
+			message: "User registered successfully",
+			type: "SUCCESS",
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Registration failed", type: "ERROR" });
+	}
+});
 
 router.post("/auth/logout", authenticateToken, (req, res) => {
 	triggerLogout(req, res);
